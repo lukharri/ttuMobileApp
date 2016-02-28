c7c9a87d681daf64ef2f6a3b53685b5f6d68afe7
lukeHarri <luke.e.harris@gmail.com>
2016-02-28 10:56:14 -0600

    Fixed gradle error - project will build but will not run on an emulator (at least not on my machine) because of lack of memory. I think this is because of the icons on the home screen. However, I did test it on an Android device and it works.
    I'm working on adding text below the icons on the home screen. I've added the first few words but commented them out because I haven't figured out how to move them where they should be.

--- app/src/main/java/androidStudio/Harris/org/MainActivity.java
+++ app/src/main/java/androidStudio/Harris/org/MainActivity.java
@@ -1,7 +1,9 @@
 package androidStudio.Harris.org;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void menuItemClicked(View view){
+        switch (view.getId()){
+            case R.id.techAnnounce:
+                Intent techAnnounce = new Intent(MainActivity.this, TechAnnounce.class);
+                startActivity(techAnnounce);
+                break;
+        }
     }
 }
